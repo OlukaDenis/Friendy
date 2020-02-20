fa46924efa0e1a1ce2019647cf7cd9d930c1fc11
OlukaDenis <olukadeno@gmail.com>
2020-02-20 23:23:36 +0300

    Added the friendship model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,46 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships, dependent: :destroy
+  has_many :reverse_friendships, class_name: 'Friendship', foreign_key: :friend_id, dependent: :destroy
+
+  # Listing all confirmed friends
+  def friends
+    friends_arr = friendships.map do |friendship|
+      friendship.friend if friendship.status
+    end
+
+    friends_arr + reverse_friendships.map do |friendship|
+      friendship.user if friendship.status
+    end
+
+    friends_arr.compact
+  end
+
+  # Outgoing friend requests
+  def outgoing_requests
+    friends_arr = friendships.map do |friendship|
+      friendship.friend if !friendship.status
+    end
+  end
+
+  # Incoming friend requests
+  def incoming_requests
+    friends_arr = reverse_friendships.map do |friendship|
+      friendship.friend if !friendship.status
+    end
+  end
+
+  # Confirming incoming friend requests
+  def confirm_friend(user)
+    friendship = reverse_friendships.find{|friendship| friendship.user == user}
+    friendship.status = true
+    friendship.save
+  end
+
+  # Check whether a user is a friend
+  def is_friend?(user)
+    friends.include?(user)
+  end
 end
